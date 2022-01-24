9bfcdb199b72847eb3b2c56e8dbccc8dbe16622a
stellantilet <stellantilet@gmail.com>
2022-01-24 08:34:29 -0800

    fix schema

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use wefund16::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-use wefund16::state::{Milestone};
+use wefund17::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use wefund17::state::{Milestone};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
